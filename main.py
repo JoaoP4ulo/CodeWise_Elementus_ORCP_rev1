1bd8028e95da328f2b3067a106f8aa1533b672ce
Joao Paulo <jppelioterio@gmail.com>
2023-04-26 13:31:28 -0300

    att final

--- main.py
+++ main.py
@@ -2,12 +2,20 @@ import os
 import pandas as pd
 import openpyxl
 
-arquivo_log = open('C:\\repository\\PR_ORCP\\CodeWise_Elementus_ORCP_rev1\\log.txt', 'r')
+#Definição de caminho
+
+#Pasta das planilhas
+caminho_planilhas = 'C:\\Users\\joao.pereira\\Elementus SA\\Elementus SA - Planejamento\\Comercial\\Propostas\\3_Aprovadas\\Input BI\\'
+termo_inicial=len(caminho_planilhas)
+#Pasta referente ao log
+caminho_log ='C:\\Users\\joao.pereira\\Elementus SA\\Elementus SA - Planejamento\\Comercial\\Propostas\\3_Aprovadas\\Controle_log_PBI'
+
+
+#Definição de variáveis globais
+arquivo_log = open(f'{caminho_log}\\log.txt', 'r')
 log = int(arquivo_log.read())
 arquivo_log.close()
-
-path = 'C:\\Users\\joao.pereira\\Elementus SA\\Elementus SA - Planejamento\\Comercial\\Propostas\\3_Aprovadas\\Input BI'
-
+path = f'{caminho_planilhas}'
 listagem = []
 data = []
 
@@ -20,16 +28,19 @@ if log == 0:
     listagem_1 = sorted(listagem_1, key=lambda arquivo: arquivo[1], reverse=True)
     listagem_1 = pd.DataFrame(listagem_1)
    
-    listagem_1.to_csv('C:\\repository\\PR_ORCP\\CodeWise_Elementus_ORCP_rev1\\historico.txt', sep=';', index=False)
+    listagem_1.to_csv(f'{caminho_log}\\historico.txt', sep=';', index=False)
    
-    
-
-
+    #leitura de arquivos na pasta
     for filename in os.listdir(path):
         if filename.endswith('.xlsx'):
             
             df = pd.read_excel(os.path.join(path, filename),sheet_name="PBI",usecols=[0,1,2,3,4,5,6])
-            projeto = str(filename)[:5]
+            
+            for v in range(len(filename)):
+                if filename[v] == "_":
+                    termo_final=v
+                    break
+            projeto = str(filename)[:termo_final]
 
             novos_termos = pd.Series([projeto] * len(df))
             df['PR'] = novos_termos
@@ -40,7 +51,7 @@ if log == 0:
     plan_PBI = pd.DataFrame(df_PBI)
 
 
-
+    #covertendo data para evitar erros
     plan_PBI['DATA'] = pd.to_datetime(plan_PBI['DATA'], errors='coerce').dt.strftime('%d/%m/%Y')
 
 
@@ -52,8 +63,9 @@ if log == 0:
         if str(linha_data)[:3] == "MÊS":
 
             plan_PBI["DATA"] = plan_PBI["DATA"].apply(lambda x: str(x).replace(str(linha_data), '') if str(x).startswith(str(linha_data)) else x)
-    plan_PBI = plan_PBI.drop(plan_PBI[plan_PBI['DATA'] == ''].index)
-    plan_PBI = pd.DataFrame(plan_PBI)
+    plan_PBI = plan_PBI.dropna(subset=['DATA'])
+    PBI_result = pd.DataFrame(plan_PBI)
+
 
 
     #Filtrando #REF nas Descrição
@@ -64,16 +76,14 @@ if log == 0:
         if str(linha_desc)[:5] == "#REF!":
 
             plan_PBI["DESCRIÇÃO"] = plan_PBI["DESCRIÇÃO"].apply(lambda x: str(x).replace(str(linha_desc), '') if str(x).startswith(str(linha_desc)) else x)
-    plan_PBI = plan_PBI.drop(plan_PBI[plan_PBI['DESCRIÇÃO'] == ''].index)  
-    plan_PBI = pd.DataFrame(plan_PBI) 
+    plan_PBI = plan_PBI.dropna(subset=['DATA'])
+    plan_PBI = plan_PBI.dropna(subset=['DESCRIÇÃO'])
+    PBI_result = pd.DataFrame(plan_PBI)
 
     
-    PBI_result = pd.DataFrame(plan_PBI)
-    PBI_result = PBI_result.drop(PBI_result[PBI_result['DATA'] == ''].index)
-    PBI_result = PBI_result.drop(PBI_result[PBI_result['DESCRIÇÃO'] == ''].index)
-    PBI_result.to_csv('C:\\repository\\PR_ORCP\\CodeWise_Elementus_ORCP_rev1\\framework_PBI.txt', sep=';', index=False)
+    PBI_result.to_csv(f'{caminho_log}\\framework_PBI.txt', sep=';', index=False)
 
-    arquivo_log = open('C:\\repository\\PR_ORCP\\CodeWise_Elementus_ORCP_rev1\\log.txt', 'w')
+    arquivo_log = open(f'{caminho_log}\\log.txt', 'w')
     arquivo_log.write("1")
     arquivo_log.close()
    
@@ -87,16 +97,17 @@ if log != 0:
     listagem_2 = sorted(listagem_2, key=lambda arquivo: arquivo[1], reverse=True)
     listagem_2 = pd.DataFrame(listagem_2)
 
-    listagem_2.to_csv('C:\\repository\\PR_ORCP\\CodeWise_Elementus_ORCP_rev1\\historico2.txt', sep=';', index=False)
+    listagem_2.to_csv(f'{caminho_log}\\historico2.txt', sep=';', index=False)
     
-    historico1 = pd.read_csv('C:\\repository\\PR_ORCP\\CodeWise_Elementus_ORCP_rev1\\historico.txt', delimiter=';')
+    historico1 = pd.read_csv(f'{caminho_log}\\historico.txt', delimiter=';')
     lista1 = historico1["0"].tolist()
-    historico2 = pd.read_csv('C:\\repository\\PR_ORCP\\CodeWise_Elementus_ORCP_rev1\\historico2.txt',delimiter=';')
+    historico2 = pd.read_csv(f'{caminho_log}\\historico2.txt',delimiter=';')
     lista2 = historico2["0"].tolist()
     
-    framework_PBI = pd.read_csv('C:\\repository\\PR_ORCP\\CodeWise_Elementus_ORCP_rev1\\framework_PBI.txt',delimiter=';')
+    framework_PBI = pd.read_csv(f'{caminho_log}\\framework_PBI.txt',delimiter=';')
     framework_PBI = pd.DataFrame(framework_PBI)
     
+    #lista arquivos com diferença entre os dois historicos
     for item in lista1:
         if item not in lista2:
             listagem.append(item)
@@ -105,13 +116,48 @@ if log != 0:
         if item not in lista1:
             listagem.append(item)
     
-    if len(lista2)>len(lista1):
         
-
+    #caso não haja mudança
+    if len(lista1)==len(lista2):
+        print("1")
+        PBI_result = pd.DataFrame(framework_PBI)
+        #historico2.to_csv(f'{caminho_log}\\historico.txt', sep=';', index=False)
+            
+            
+    #caso haja a remoção de arquivo da pasta
+    if len(lista1)>len(lista2):
+        print("2")
+        
+        for filename in listagem:
+            
+            for v in range(termo_inicial,len(filename)):
+                if filename[v] == "_":
+                    termo_final=v
+                    break
+            
+            projeto = str(filename)[termo_inicial:termo_final]
+                    
+            framework_PBI = framework_PBI.drop(framework_PBI[framework_PBI['PR'] == projeto].index)
+            framework_PBI = pd.DataFrame(framework_PBI)
+        PBI_result=pd.DataFrame(framework_PBI)     
+        PBI_result.to_csv(f'{caminho_log}\\framework_PBI.txt', sep=';', index=False)
+        historico2.to_csv(f'{caminho_log}\\historico.txt', sep=';', index=False)
+        
+    
+    #caso haja novo arquivo 
+    if len(lista2)>len(lista1):
+        print("3")
+      
         for filename in listagem:
         
             df = pd.read_excel(filename,sheet_name="PBI",usecols=[0,1,2,3,4,5,6])
-            projeto = str(filename)[104:109]
+            
+            for v in range(termo_inicial,len(filename)):
+                if filename[v] == "_":
+                    termo_final=v
+                    break
+            
+            projeto = str(filename)[termo_inicial:termo_final]
 
             novos_termos = pd.Series([projeto] * len(df))
             df['PR'] = novos_termos
@@ -137,7 +183,8 @@ if log != 0:
                 if str(linha_data)[:3] == "MÊS":
 
                     plan_PBI["DATA"] = plan_PBI["DATA"].apply(lambda x: str(x).replace(str(linha_data), '') if str(x).startswith(str(linha_data)) else x)
-            plan_PBI = plan_PBI.drop(plan_PBI[plan_PBI['DATA'] == ''].index)
+            plan_PBI = plan_PBI.dropna(subset=['DATA'])
+            
             plan_PBI = pd.DataFrame(plan_PBI)
 
             #Filtrando #REF nas Descrição
@@ -148,28 +195,18 @@ if log != 0:
                 if str(linha_desc)[:5] == "#REF!":
                     
                     plan_PBI["DESCRIÇÃO"] = plan_PBI["DESCRIÇÃO"].apply(lambda x: str(x).replace(str(linha_desc), '') if str(x).startswith(str(linha_desc)) else x)
-            plan_PBI = plan_PBI.drop(plan_PBI[plan_PBI['DESCRIÇÃO'] == ''].index)
-            plan_PBI = pd.DataFrame(plan_PBI)
+            plan_PBI = plan_PBI.dropna(subset=['DATA'])
+            plan_PBI = plan_PBI.dropna(subset=['DESCRIÇÃO'])
 
             PBI_result = pd.concat([framework_PBI,plan_PBI])
             PBI_result = pd.DataFrame(PBI_result)
             
-            PBI_result = PBI_result.drop(PBI_result[PBI_result['DATA'] == ''].index)
-            PBI_result = PBI_result.drop(PBI_result[PBI_result['DESCRIÇÃO'] == ''].index)
-            PBI_result.to_csv('C:\\repository\\PR_ORCP\\CodeWise_Elementus_ORCP_rev1\\framework_PBI.txt', sep=';', index=False)
-    
-    if len(lista1)>len(lista2):
-        for filename in listagem:
-            
-            projeto = str(filename)[104:109]
-            
-            framework_PBI = framework_PBI.drop(framework_PBI[framework_PBI['PR'] == projeto].index)
-            PBI_result = pd.DataFrame(framework_PBI)
-        PBI_result.to_csv('C:\\repository\\PR_ORCP\\CodeWise_Elementus_ORCP_rev1\\framework_PBI.txt', sep=';', index=False)
-    
-    if len(lista1) == len(lista2):
-        PBI_result = pd.DataFrame(framework_PBI)
-    
-    
-    historico2.to_csv('C:\\repository\\PR_ORCP\\CodeWise_Elementus_ORCP_rev1\\historico1.txt', sep=';', index=False)
-    
+            PBI_result.to_csv(f'{caminho_log}\\framework_PBI.txt', sep=';', index=False)
+            historico2.to_csv(f'{caminho_log}\\historico.txt', sep=';', index=False)
+
+
+PBI_result = PBI_result.dropna(subset=['DATA'])
+PBI_result = PBI_result.dropna(subset=['DESCRIÇÃO'])
+PBI_result = pd.DataFrame(PBI_result)
+PBI_result = PBI_result.drop_duplicates()
+PBI_result = pd.DataFrame(PBI_result)
--- readme.txt
+++ readme.txt
@@ -1,16 +1,32 @@
 
 Mudanças de pratica:
-    - DATAS: ajustar as datas na planilha (só deixar as datas dos meses usuais)
+    - DATAS: ajustar as datas na planilha (só deixar as datas dos meses usuais) - no segundo calendario de Despesas Indiretas
 
-
-A fazer:
-    - FILTRAR:, imposto, margem, overhead, receita, recebimentos, risco, total mensal, total acumulado
-
-    - FUNÇOES: conferir valores, começar da ultima mudança
-
-Estudar: 
-    - 
-
-Erros:
-    - Identificar aditivos ADXX
+Tratamentos no PBI:
     - Filtra coluna "VALOR" para -> mudar <type> para texto -> substituir "." para "," -> mudar <type> para valor, float
+
+Arquitetura:
+    - os quatro arquivos .txt são arquivos de controle para otimizar o processo
+
+log.txt ->  0 - framework_PBI não gerado (primeira vez) / 
+            1 - framework_PBI existe, seguir tratativas
+
+historico.txt -> primeiro historico rodado em log=0 e alerado após toda execução com o valor de historico2
+
+historico2.txt -> historico auxiliar que lê o conteudo atual da pasta e confere divergências com o primeiro historico
+
+framework_PBI.txt -> DataFrame principal salvo em txt para evitar a releitur de arquivo já otimizaados 
+
+IMPORTANTE: 
+    - não apagar o arquivo "log.txt"
+    - para resetar a quarry e reconferir todos os valores mudar log para 1
+
+
+
+PBI_result -> tabela a ser puxada no PowerBI
+
+
+Funções possíveis mas o JP não tinha tempo : 
+    - Enviar um relatório por email de erros ou inconsitência nos valores
+    - Caso o erro seja com relação a exclusão de linhas, criar uma função para completar as linhas faltantes (manipulação de arquivo)
+
